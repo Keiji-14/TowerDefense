3e0ceac6e1648a2c55c8a5f39b51d0b68c1cd3f9
足利 慶次 <a.keiji14@gmail.com>
2024-05-07 23:36:43 +0900

    ゲームの情報を保持・管理するクラスを追加

---
+++ TowerDefense/Assets/Script/Game/GameDataManager.cs
@@ -0,0 +1,71 @@
+﻿using GameData.Enemy;
+using GameData.Stage;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+    public class GameDataManager : MonoBehaviour
+    {
+        #region PublicField
+        public static GameDataManager instance = null;
+        #endregion
+
+        #region PrivateField
+        /// <summary>ゲームの情報</summary>
+        private GameDataInfo gameDataInfo;
+        /// <summary>ステージの情報</summary>
+        [SerializeField] private StageDataInfo stageDataInfo;
+        #endregion
+
+        #region UnityEvent
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        #endregion
+
+        #region PublicMethod
+        /// <summary>
+        /// ゲームの情報を設定する
+        /// </summary>
+        public void SetGameDataInfo(GameDataInfo gameDataInfo)
+        {
+            this.gameDataInfo = gameDataInfo;
+        }
+
+        /// <summary>
+        /// ゲームの情報を返す
+        /// </summary>
+        public GameDataInfo GetGameDataInfo()
+        {
+            return gameDataInfo;
+        }
+
+        /// <summary>
+        /// ステージの情報を設定する
+        /// </summary>
+        public void SetStageDataInfo(StageDataInfo stageDataInfo)
+        {
+            this.stageDataInfo = stageDataInfo;
+        }
+
+        /// <summary>
+        /// ステージの情報を返す
+        /// </summary>
+        public StageDataInfo GetStageDataInfo()
+        {
+            return stageDataInfo;
+        }
+        #endregion
+    }
+}
