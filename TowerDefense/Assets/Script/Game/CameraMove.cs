f063dee74203369ebf5386b28c08009524483c2b
足利 慶次 <a.keiji14@gmail.com>
2024-05-09 04:11:48 +0900

    カメラの操作処理を追加

---
+++ TowerDefense/Assets/Script/Game/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class CameraMove : MonoBehaviour
+    {
+        #region SerializeField 
+        /// <summary>カメラの移動速度</summary>
+        [SerializeField] private float moveSpeed;
+        #endregion
+
+        #region UnityEvent
+        void Update()
+        {
+            // 入力を取得
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            
+            // 入力値に応じて移動させる
+            Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+            
+            // カメラの方向に合わせて移動させる
+            movement = transform.TransformDirection(movement);
+            movement.y = 0f; // Y方向の移動は無視する
+            
+            // カメラの水平移動
+            transform.position += movement;
+        }
+        #endregion
+    }
+}
