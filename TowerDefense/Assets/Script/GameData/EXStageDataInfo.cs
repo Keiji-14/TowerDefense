dea8063c9ae44e84b2edc55d731712eceaef51dc
足利 慶次 <a.keiji14@gmail.com>
2024-05-20 01:13:08 +0900

    EXステージのプレハブオブジェクトを追加

---
+++ TowerDefense/Assets/Script/GameData/EXStageDataInfo.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData.Stage
+{
+    /// <summary>
+    /// EXステージの情報を保持するScriptableObject
+    /// </summary>
+    [CreateAssetMenu(fileName = "EXStageData", menuName = "Create EX Stage Data")]
+    public class EXStageDataInfo : ScriptableObject
+    {
+        #region PublicField
+        /// <summary>ステージID</summary>
+        public int stageID;
+        /// <summary>開始時の砦の耐久値</summary>
+        public int startFortressLife;
+        /// <summary>開始時の所持金</summary>
+        public int startMoney;
+        /// <summary>ウェーブのインターバル</summary>
+        public float waveInterval;
+        /// <summary>ステージのオブジェクト</summary>
+        public GameObject stageObj;
+        /// <summary>砦の場所</summary>
+        public Transform fortressTransform;
+        /// <summary>EXステージのルートの情報</summary>
+        public List<RouteInfo> routeInfoList = new List<RouteInfo>();
+        #endregion
+    }
+
+    /// <summary>
+    /// EXステージのルートの情報
+    /// </summary>
+    [System.Serializable]
+    public class RouteInfo
+    {
+        #region PublicField
+        /// <summary>敵の出現数</summary>
+        //public List<EnemySpawnInfo> enemySpawnInfoList = new List<EnemySpawnInfo>();
+        /// <summary>敵の出現場所</summary>
+        public Transform spawnPoint;
+        /// <summary>ルートの中継地点</summary>
+        public List<Transform> routeAnchor;
+        #endregion
+    }
+}
