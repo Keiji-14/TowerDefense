fa22b6f78805ca3527b472bf946364280d9b374c
足利 慶次 <a.keiji14@gmail.com>
2024-05-27 06:37:03 +0900

    ジャミングタワーに簡易アニメーションを追加

---
+++ TowerDefense/Assets/Script/Direction/JammingTower.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Direction
+{
+    /// <summary>
+    /// ジャミングタワーの演出
+    /// </summary>
+    public class JammingTower : MonoBehaviour
+    {
+        #region SerializeField
+        /// <summary>回転速度</summary>
+        [SerializeField] float rotateSpeed;
+        /// <summary>ジャミングタワーの外周オブジェクト</summary>
+        [SerializeField] private GameObject haloObj;
+        #endregion
+
+        #region UnityEvent
+        void Update()
+        {
+            RotateHalo();
+        }
+        #endregion
+
+        #region PrivateMethod
+        /// <summary>
+        /// 回転させる処理速度
+        /// </summary>
+        private void RotateHalo()
+        {
+            if (haloObj != null)
+            {
+                haloObj.transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+            }
+        }
+        #endregion
+    }
+}
