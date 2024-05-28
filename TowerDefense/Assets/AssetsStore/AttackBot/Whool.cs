3df45c34ae871720970e86fe737ed140736d9dea
足利 慶次 <a.keiji14@gmail.com>
2024-05-28 11:36:02 +0900

    敵の種類を追加

---
+++ TowerDefense/Assets/AssetsStore/AttackBot/Whool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whool : MonoBehaviour
+{
+    #region SerializeField
+    /// <summary>回転速度</summary>
+    [SerializeField] float rotateSpeed;
+    /// <summary>ジャミングタワーの外周オブジェクト</summary>
+    [SerializeField] private GameObject whoolObj;
+    #endregion
+
+    #region UnityEvent
+    void Update()
+    {
+        RotateHalo();
+    }
+    #endregion
+
+    #region PrivateMethod
+    /// <summary>
+    /// 回転させる処理速度
+    /// </summary>
+    private void RotateHalo()
+    {
+        if (whoolObj != null)
+        {
+            whoolObj.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+        }
+    }
+    #endregion
+}
